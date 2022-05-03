d29aa017ebdb797c9d8cd5d1979a5a17350a7865
javerpg <hot69carolina@gmail.com>
2022-05-03 17:27:11 +0000

    Añadido esqueleto Spring Boot

---
+++ calculator33/src/main/java/com/ciber33/calculator33/Calculator33Application.java
@@ -0,0 +1,13 @@
+package com.ciber33.calculator33;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Calculator33Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Calculator33Application.class, args);
+	}
+
+}
