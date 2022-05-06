b345370ce9de8bf346ad84e66e57c2fb25ddc1c0
javerpg <hot69carolina@gmail.com>
2022-05-06 16:15:14 +0000

    Se añaden suma, controlador y test unitario

--- src/test/java/com/ciber33/calculator33/Calculator33ApplicationTests.java
+++ src/test/java/com/ciber33/calculator33/Calculator33ApplicationTests.java
@@ -3,11 +3,19 @@ package com.ciber33.calculator33;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.Assert.assertEquals;
+
 @SpringBootTest
 class Calculator33ApplicationTests {
+
+        private Calculator calculator = new Calculator();
 
 	@Test
 	void contextLoads() {
 	}
 
+        @Test
+        public void testSum() {
+            assertEquals(5, calculator.sum(2, 3));
+        }
 }
