97f58fcea5a958ba2c5cebd9c95f34faa4892c1d
javerpg <hot69carolina@gmail.com>
2022-05-17 18:10:01 +0000

    Añadido Jenkinsfile

--- src/main/java/com/ciber33/calculator33/Calculator33Application.java
+++ src/main/java/com/ciber33/calculator33/Calculator33Application.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Calculator33Application {
+ public class Calculator33Application {
 
  public static void main(String[] args) {
-  SpringApplication.run(Calculator33Application.class, args);
+   SpringApplication.run(Calculator33Application.class, args);
  }
 }
